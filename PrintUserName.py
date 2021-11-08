d4ca4f3c7b251a3cbe0e9fbece45a14692884886
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-08 21:25:38 +0530

    [Ravina] Added : Function to print the the user Name with message.

---
+++ PrintUserName.py
@@ -0,0 +1,15 @@
+def printName() :
+
+ UserName = input("Enter the name : ")
+
+ checkLength = len(UserName)
+
+ if (checkLength >= 3) :
+          print ("Hello " + UserName + "  How are you?")
+ else :
+     print ("Username should have atleast three characters")
+     printName()
+
+
+
+printName()
