b531f51316bbb489a77cf631724382c4f4a24c13
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-09 00:53:46 +0530

    [Ravina] Added : Function to print the user name with message.

--- PrintUserName.py
+++ PrintUserName.py
@@ -1,3 +1,14 @@
+
+"""
+@Author: Ravina Maudekar
+@Date: 2021-08-11 8:26:00
+@Last Modified by: Ravina Maudekar
+@Last Modified time: 2021-08-11 12:24:24
+@Title : To print the username with message.
+
+""" 
+
+
 def printName() :
 
  UserName = input("Enter the name : ")
