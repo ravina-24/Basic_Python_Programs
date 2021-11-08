6f709b84d33d417e828d5fc8e904e30176d28ea7
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-09 00:42:08 +0530

    [Ravina] Added : Program to find the percentage of head and tail.

--- FlipCoin.py
+++ FlipCoin.py
@@ -1,4 +1,13 @@
 
+
+"""
+@Author: Ravina Maudekar
+@Date: 2021-08-11 8:00:24
+@Last Modified by: Ravina Maudekar
+@Last Modified time: 2021-08-11 12:00:35
+@Title : To print the percentage of head and tail 
+
+""" 
 TakeInput = int(input("Enter how many times you want to flip the coin : "))
 
 i =1
