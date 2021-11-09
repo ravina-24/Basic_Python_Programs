63cdd12cfc0d766a5187af7f317d7e3e539ba60f
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-09 13:55:15 +0530

    [Ravina] Added : Program to print the prime factors of a number.

---
+++ PrimeFactors.py
@@ -0,0 +1,22 @@
+"""
+@Author: Ravina Maudekar
+@Date: 2021-09-11 13:00:24
+@Last Modified by: Ravina Maudekar
+@Last Modified time: 2021-09-11 13:00:24
+@Title : To print the prime factors of a number.
+
+""" 
+
+print("Finding the prime factors of a number \n")
+
+Num = int(input("Enter the number : "))
+i=2
+print("Prime factors are")
+while(i<=Num and Num !=0 ) :
+    
+    if Num%i == 0 :
+         print(i) 
+
+    i+=1
+
+
