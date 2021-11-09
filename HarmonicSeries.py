213b59f4ad66da69b92b77812bedc14300e322de
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-09 10:52:04 +0530

    [Ravina] Added : Program to print the harmonic series and it's sum.

--- HarmonicSeries.py
+++ HarmonicSeries.py
@@ -15,13 +15,10 @@ harmonicseries = 0
 print("Harmonic series : ")
 while (i<=Num and Num !=0 ) :
 
-    
-
     print ("{}/{} +".format(1,i) ,end=" ")
    
     harmonicseries =harmonicseries+ 1/i 
-
-    
+      
     i+=1
 
 
