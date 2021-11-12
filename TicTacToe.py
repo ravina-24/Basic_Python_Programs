ed68be5f457e979409658f88a6b2babcee5e1985
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-12 21:07:11 +0530

    [Ravina] Added : Program to play the game of tic tac toe.

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,12 +2,14 @@
 '''
 @Author: Ravina Maudekar
 @Date: 2021-08-20 20:00:00
-@Title : Write a Program to play a Cross Game or Tic-Tac-Toe Game.
+@Title : Write a Program to play a Cross Game or Tic-Tac-Toe Game. Player 2
+
 '''
 
 import random
 
 square = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 def main():
     """
@@ -33,8 +35,8 @@ def main():
             choice = int(input('Enter a number:'))
             mark = 'X'
         else:
-            choice=random.randrange(1,9)
-            print("Value Choose by Computer : ",choice)
+            choice = random.randrange(1, 9)
+            print("Value Choose by Computer : ", choice)
             mark = 'O'
 
         if choice == 1 and square[1] == 1:
@@ -93,10 +95,11 @@ def game_status():
     elif square[3] == square[5] and square[5] == square[7]:
         return 1
     elif square[1] != 1 and square[2] != 2 and square[3] != 3 and square[4] != 4 and square[5] != 5 and square[
-        6] != 6 and square[7] != 7 and square[8] != 8 and square[9] != 9:
+            6] != 6 and square[7] != 7 and square[8] != 8 and square[9] != 9:
         return 0
     else:
         return -1
+
 
 def board():
     """
@@ -114,5 +117,6 @@ def board():
     print('     |     |     ')
 
     print(' ', square[7], ' | ', square[8], ' |  ', square[9])
+
 
 main()
