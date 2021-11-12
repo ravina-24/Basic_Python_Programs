68d6d6ac6299cfdf2d5da2b5254895b275cece69
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-12 21:17:25 +0530

    [Ravina] Added : Program to print the ecludean distance between x,y a…

--- Distance.py
+++ Distance.py
@@ -3,13 +3,14 @@
 @Date: 2021-10-11 9:22:00
 @Last Modified by: Ravina Maudekar
 @Title : To calculate the ecludean distance between x , y and origin .
-
 """
+
+import math
 try:
     x = float(input("Enter value of x : "))
     y = float(input("Enter value of y : "))
 
-    import math
+    
     ecludeanDistance =math.sqrt(x*x+y*y)
     print("Ecludean distance  = {} ".format(ecludeanDistance))
 
