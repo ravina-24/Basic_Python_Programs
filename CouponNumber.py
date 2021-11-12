bd5cebaa2d0f3d5544e981a116a528984abbb6b2
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-12 21:09:24 +0530

    [Ravina] Added : [Ravina] Added : Program to find the distinct coupon number.

--- CouponNumber.py
+++ CouponNumber.py
@@ -4,34 +4,37 @@
 @Last Modified by: Ravina Maudekar
 @Title : To find the distinct coupon numbers .
 
-""" 
+"""
 from array import*
 from random import randint
-class coupon:
-    
-    def findDistinctCoupon(self):
-     print("-------------Coupon Number --------------")
 
-    Num = int(input("Enter the Coupon number : "))  
-    set =[]
-    
-    maxLimit =int(input("Enter max limit : "))
+
+class coupon:
+
+    def findDistinctCoupon(self):
+        print()
+
+    Num = int(input("Enter the Coupon number : "))
+    set = []
+
+    maxLimit = int(input("Enter max limit : "))
     count = 0
-    i=1
+    i = 1
     isRepeated = True
-    for i in range(Num) :
+    for i in range(Num):
         import random
-        randomNum = randint(0,maxLimit)
-        if randomNum not in set :
-              set.append(randomNum)
-              count+=1
-        elif randomNum in set :
-           isRepeated = False    
+        randomNum = randint(0, maxLimit)
+        if randomNum not in set:
+            set.append(randomNum)
+            count += 1
+        elif randomNum in set:
+            isRepeated = False
         else:
             print("Does not contain any distict  number")
 
-    print ("Distinct coupon number {}: ".format(count))
+    print("Distinct coupon number {}: ".format(count))
     print(set)
 
+
 c = coupon()
-c.findDistinctCoupon()        
+c.findDistinctCoupon()
