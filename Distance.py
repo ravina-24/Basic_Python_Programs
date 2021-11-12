57658949e69efca15bf7b4e2be90e5376b5af089
ravina-24 <ravinamaudekar24@gmail.com>
2021-11-12 21:24:51 +0530

    [Ravina] Added : Program to print the ecludean distance between x,y and orgin

--- Distance.py
+++ Distance.py
@@ -10,10 +10,11 @@ import math
 try:
     x = float(input("Enter value of x : "))
     y = float(input("Enter value of y : "))
-    
+
     ecludeanDistance =math.sqrt(x*x+y*y)
     print("Ecludean distance  = {} ".format(ecludeanDistance))
 
+    
 except ValueError:
     print("Sorry, you can not enter string value ! ")
      
